e804a81a8289a48b5b5417609eb872296e6f36e5
boom <bhargavahire910@gmail.com>
2025-11-10 12:52:50 +0530

    Update 2Bmain.c

--- 2Bmain.c
+++ 2Bmain.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -5,23 +6,30 @@
 #include <unistd.h>
 #include <string.h>
 
-// Comparison function for qsort (built-in is easier here)
-int compare(const void * a, const void * b) {
-    return (*(int*)a - *(int*)b);
+// --- Bubble Sort Function ---
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
-
 int main() {
     int n;
     printf("Enter number of integers: ");
     if (scanf("%d", &n) != 1) return 1;
-    
-    // Using VLA for simplicity
     int arr[n];
     printf("Enter %d integers: ", n);
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-    // Parent sorts array first
-    qsort(arr, n, sizeof(int), compare);
+    // Parent sorts array first using Bubble Sort
+    bubbleSort(arr, n);
+
     printf("\n[PARENT] Sorted array: ");
     for(int i=0; i<n; i++) printf("%d ", arr[i]);
     printf("\n");
@@ -31,31 +39,23 @@ int main() {
     if (pid == 0) {
         // --- CHILD PROCESS ---
         printf("[CHILD] I will now EXECVE into the 'reverse' program.\n");
-
-        // Prepare arguments for execve
-        // We need: program name + n numbers + NULL terminator = n + 2 slots
         char *args[n + 2];
         args[0] = "./rever"; 
 
-        // Convert integers to strings for command line args
         for (int i = 0; i < n; i++) {
-            args[i+1] = malloc(12); // 12 bytes is enough for a 32-bit int string
+            args[i+1] = malloc(12);
             sprintf(args[i+1], "%d", arr[i]);
         }
-        args[n+1] = NULL; // List must end with NULL
+        args[n+1] = NULL;
 
-        // Replace current process with ./reverse
-        // IF THIS FAILS: make sure you compiled reverse.c into an executable named 'reverse'
-        // and it is in the SAME folder as this program.
         execve(args[0], args, NULL);
         
-        // If we get here, execve failed!
         perror("execve failed");
         exit(1);
     } 
     else if (pid > 0) {
         // --- PARENT PROCESS ---
-        wait(NULL); // Wait for child (which is now the 'reverse' program) to finish
+        wait(NULL);
         printf("[PARENT] Child finished. Main program exiting.\n");
     }
     else {
